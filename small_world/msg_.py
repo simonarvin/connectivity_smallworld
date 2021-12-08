0bd92c3be7a7b17385fa13962ce87de3c4a3b7c3
Simon Arvin <40764374+simonarvin@users.noreply.github.com>
2021-12-08 15:10:02 +0100

    Update msg_.py

--- small_world/msg_.py
+++ small_world/msg_.py
@@ -1,6 +1,6 @@
 import time
 title = "'Short- and long-range connections differentially modulate the small-world network’s dynamics and state'"
-doi = "doi: TBA"
+doi = "doi: 10.3389/fncom.2021.783474"
 authors = "Simon Arvin ^1,2, Andreas Nørgaard Glud ^2 and Keisuke Yonehara 1"
 cense = "Center for Experimental Neurosurgery, Aarhus University Hospital"
 dandrite = "Danish Research Institute of Translational Neuroscience"
